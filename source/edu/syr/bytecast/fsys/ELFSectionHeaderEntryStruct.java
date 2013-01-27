3969630ca32642904982a4a2956a1835d0df3b13
Shawn Mueller <smueller@syr.edu>
2013-01-27 15:20:24 -0500

    Fixed missing argument in IBytecastFsys. Added ELFSectionHeaderEntryStruct.

--- source/edu/syr/bytecast/fsys/ELFSectionHeaderEntryStruct.java
+++ source/edu/syr/bytecast/fsys/ELFSectionHeaderEntryStruct.java
@@ -1,5 +1,14 @@
 package edu.syr.bytecast.fsys;
 
 public class ELFSectionHeaderEntryStruct {
-    
+    int     sh_name;
+    int     sh_type;
+    int     sh_link;
+    int     sh_info;
+    long    sh_flags;
+    long    sh_addr;
+    long    sh_offset;
+    long    sh_size;
+    long    sh_addralign;
+    long    sh_entsize;
 }
--- source/edu/syr/bytecast/fsys/IBytecastFsys.java
+++ source/edu/syr/bytecast/fsys/IBytecastFsys.java
@@ -2,7 +2,7 @@ package edu.syr.bytecast.fsys;
 import java.io.IOException;
 
 public interface IBytecastFsys {
-    public void setFilepath();
+    public void setFilepath(String file_path);
     public String getFilepath();
     ExeObj parse() throws IOException;
 }
