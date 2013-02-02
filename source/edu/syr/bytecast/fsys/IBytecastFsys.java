87a5a64f706e43158bac74f1276da20f8eba706e
Aaron <amdodds@syr.edu>
2013-02-02 17:18:23 -0500

    deleted source folder

--- source/edu/syr/bytecast/fsys/IBytecastFsys.java
+++
@@ -1,8 +0,0 @@
-package edu.syr.bytecast.fsys;
-import java.io.IOException;
-
-public interface IBytecastFsys {
-    public void setFilepath(String file_path);
-    public String getFilepath();
-    public ExeObj parse() throws IOException;
-}
