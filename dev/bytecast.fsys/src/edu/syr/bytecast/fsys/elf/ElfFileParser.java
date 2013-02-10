12fab8ddd59143e5b7a6536aeced2d93528f36d4
Aaron <amdodds@syr.edu>
2013-02-10 16:20:06 -0500

    Create ElfFileParser class

---
+++ dev/bytecast.fsys/src/edu/syr/bytecast/fsys/elf/ElfFileParser.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.syr.bytecast.fsys.elf;
+import java.util.*;
+
+/**
+ *
+ * @author adodds
+ */
+public class ElfFileParser {
+    
+    private String m_filePath;
+    private List<List<String>> m_sections;
+    private List<List<String>> m_segments;
+    
+    public void setFilePath(String filePath)
+    {
+        m_filePath = filePath;
+    }
+    public String getFilePath()
+    {
+        return m_filePath;  
+    }
+    public boolean attach()
+    {return false;}
+    public boolean detach()
+    {return false;}
+    public ElfElfHeaderStruct getElfHeader()
+    {return null;}
+    public ElfProgramHeaderStruct getProgramHeader()
+    {return null;}
+    public ElfSectionHeaderStruct getSectionHeader()
+    {return null;};
+    public List<List<Byte>> getSections()
+    {return null;}
+    public List<List<Byte>> getSegments()
+    {return null;}
+}
